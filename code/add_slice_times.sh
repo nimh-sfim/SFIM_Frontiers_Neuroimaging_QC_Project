fa40f8d163b8a1b6e41d1808ed74f9ab82a829d2
Joshua Teves <joshua.teves@nih.gov>
2022-08-29 16:24:19 -0400

    Adds slice time code

---
+++ code/add_slice_times.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+# Add slice timing information to epi files
+
+abids_loc=$(which -s abids_tool.py)
+
+if [[ $abids_loc =~ "not found" ]]
+then
+    echo "Cannot find abids_tool.py on path."
+    echo "Is AFNI properly installed?"
+    exit 1
+fi
+
+for i in {1..30}
+do
+    sid=$(printf "%3.3d" $i)
+    epi=$($QC_CODE_ROOT/rawloc.sh $sid epi)
+    abids_tool.py \
+        -input $epi \
+        -add_slice_times
+    if [[ ! $? ]]
+    then
+        echo "Encountered error; exiting..."
+        exit 1
+    fi
+done
+
+for i in {101..120}
+do
+    sid=$(printf "%3.3d" $i)
+    epi=$($QC_CODE_ROOT/rawloc.sh $sid epi)
+    abids_tool.py \
+        -input $epi \
+        -add_slice_times
+    if [[ ! $? ]]
+    then
+        echo "Encountered error; exiting..."
+        exit 1
+    fi
+
+done
